eb51f282b79891835c596079e1deafa0d82d8c59
Anderson Vantini - 823134667 <823134667@ulife.com.br>
2023-05-03 20:03:08 -0300

    Primeiro commit

---
+++ ClassePrimaria.java
@@ -0,0 +1,34 @@
+public class ClassePrimaria {
+    private String nome;
+    private int cpf;
+    private int senha;
+
+public ClassePrimaria (String nome,int cpf, int senha ) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.senha = senha;
+
+}
+
+public class getNome (){
+    return = nome;
+
+}
+public class getcpf (){
+    return = cpf;
+   
+}
+public class getsenha(){
+
+}
+public void setNome (){
+
+}
+public void setcpf(){
+
+}
+public void setsenha(){
+    
+}
+
+}
